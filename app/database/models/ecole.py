afd392f8816ce4d00a0a7a8f816b457806d36ac7
adoumasseo <adoumasseo@gmail.com>
2025-01-06 10:12:47 +0100

    config: ecoles et university models

---
+++ app/database/models/ecole.py
@@ -0,0 +1,38 @@
+from sqlalchemy import String, ForeignKey, DateTime
+from sqlalchemy.orm import mapped_column, relationship
+from app.extensions import db
+from app.database.models.university import Universite
+from uuid import uuid4
+from datetime import datetime
+
+class Ecole(db.Model):
+    """ Model of table ecole
+    """
+    __tablename__ = 'ecoles'
+    id_ecole = mapped_column(String(128), primary_key=True, nullable=False)
+    nom = mapped_column(String(10), nullable=False)
+    code= mapped_column(String(10), nullable=False)
+    id_universite = mapped_column(String(128), ForeignKey(Universite.id_universite), nullable=False)
+    code = mapped_column(String(128), nullable=True)
+    created_at = mapped_column(DateTime, default=datetime.now())
+    updated_at = mapped_column(DateTime, default=datetime.now())
+    deleted_at = mapped_column(DateTime, default=None)
+    universite = relationship("Universite", back_populates="ecoles")
+
+    def __init__(self, nom, code):
+        """Initiate the model object with column values
+        """
+        self.id_ecole = str(uuid4())
+        self.nom = nom
+        self.code = code
+        self.created_at = datetime.now()
+
+    def __str__(self):
+        """Return a string representation of a serie
+        """
+        return "Ecole: {}".format(self.nom)
+
+    def __repr__(self):
+        """Return a string representation of a serie
+        """
+        return "Ecole: {}".format(self.nom)
---
+++ app/database/models/university.py
@@ -0,0 +1,35 @@
+from sqlalchemy import String, DateTime
+from sqlalchemy.orm import mapped_column, relationship
+from app.extensions import db
+from uuid import uuid4
+from datetime import datetime
+
+class Universite(db.Model):
+    """ Model of table university
+    """
+    __tablename__ = 'universities'
+    id_universite = mapped_column(String(128), primary_key=True, nullable=False)
+    nom = mapped_column(String(10), nullable=False)
+    code = mapped_column(String(10), nullable=False)
+    created_at = mapped_column(DateTime, default=datetime.now())
+    updated_at = mapped_column(DateTime, default=datetime.now())
+    deleted_at = mapped_column(DateTime, default=None)
+    ecoles = relationship("Ecole", back_populates="university")
+
+    def __init__(self, nom, code):
+        """Initiate the model object with column values
+        """
+        self.id_universite = str(uuid4())
+        self.nom = nom
+        self.code = code
+        self.created_at = datetime.now()
+
+    def __str__(self):
+        """Return a string representation of a serie
+        """
+        return "Universite: {}, Code: {}".format(self.nom, self.code)
+
+    def __repr__(self):
+        """Return a string representation of a serie
+        """
+        return "Universite: {}, Code: {}".format(self.nom, self.code)
