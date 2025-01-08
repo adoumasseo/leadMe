44383b68bcda9dd7b8e2b770b1a00d495090c10d
adoumasseo <adoumasseo@gmail.com>
2025-01-08 10:35:04 +0100

    Add seeder logic with click and flask_cli, fixing migrations and models

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from config import Config
 from flask_migrate import Migrate
 from app.extensions import db
+from flask.cli import with_appcontext
+import click
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -14,6 +16,17 @@ def create_app(config_class=Config):
     # Register blueprints here
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
+    
+    # CLI for the app
+    # Add CLI commands
+    @app.cli.command("seed")
+    @with_appcontext
+    def seed():
+        """Seed the database with sample data."""
+        from app.database.seeds import seed_users
+        seed_users.seed_users()
+        click.echo("Database seeded successfully!")
+    
     @app.route('/test/')
     def test_page():
         return '<h1>Testing the Flask Application Factory Pattern</h1>'
---
+++ app/database/seeds/seed_users.py
@@ -0,0 +1,21 @@
+from app.extensions import db
+from app.database.models.user import User
+from faker import Faker
+
+fake = Faker()
+
+def seed_users():
+    """Function to seed the database with sample users."""
+    users = [
+        User(
+            prenom="ADMIN",
+            nom="ADMIN",
+            matricule=None,
+            email=fake.email(),
+            password=fake.password(),
+            role="admin"
+        )
+    ]
+    db.session.bulk_save_objects(users)
+    db.session.commit()
+    print("Seeded users table successfully!")
