d4efc570bfe63deb8aa8e846c049d0bda85548a2
adoumasseo <adoumasseo@gmail.com>
2025-01-10 12:50:16 +0100

    feat: env.example and config.py update to include gmail smtp server

--- config.py
+++ config.py
@@ -6,3 +6,9 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(os.getcwd(), 'app/static/uploads')
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+    MAIL_SERVER = os.getenv('MAIL_SERVER', '')
+    MAIL_PORT = os.getenv('MAIL_PORT', 587)
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME', '')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD', '')
+    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS', True)
+    MAIL_USE_SSL = os.getenv('MAIL_USE_SSL', False)
