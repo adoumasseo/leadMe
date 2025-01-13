f665b696c516bffaf3fd3ac27071a74bb091b4a1
adoumasseo <adoumasseo@gmail.com>
2025-01-13 08:59:19 +0100

    fix: before a t oken is generate if an old one with the same key exist it is delete first the code expiration is decrease from 5min 1min

--- app/utils/code.py
+++ app/utils/code.py
@@ -1,7 +1,7 @@
 """" A module for generate a code, store it in redis and send a mail"""
 
-import random, os
-from redis_config import redis_client
+import random
+from redis_config import redis_client, RESET_CODE_EXPIRATION
 
 def generate_reset_code():
     """Generate a 6-digit verification code."""
@@ -10,5 +10,8 @@ def generate_reset_code():
 def store_reset_code(email):
     """Store the reset code in Redis with an expiration time."""
     reset_code = generate_reset_code()
-    redis_client.setex(f"reset_code:{email}", int(os.getenv('RESET_CODE_EXPIRATION', 300)), reset_code.encode('utf-8'))
+    old_code = redis_client.get(f"reset_code:{email}")
+    if old_code:
+        redis_client.delete(f"reset_code:{email}")
+    redis_client.setex(f"reset_code:{email}", RESET_CODE_EXPIRATION, reset_code.encode('utf-8'))
     return reset_code
--- redis_config.py
+++ redis_config.py
@@ -2,4 +2,4 @@ import redis
 
 # Configure Redis
 redis_client = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
-RESET_CODE_EXPIRATION = 300  # 5 minutes in seconds
+RESET_CODE_EXPIRATION = 60  # 5 minutes in seconds
