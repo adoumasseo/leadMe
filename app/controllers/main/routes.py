8f2698b9bbfbc818a3246f0f01f38710b530d0af
adoumasseo <adoumasseo@gmail.com>
2025-01-22 11:31:12 +0100

    feat: Update landing page

--- app/controllers/main/routes.py
+++ app/controllers/main/routes.py
@@ -1,8 +1,10 @@
 
 from app.controllers.main import bp
 from flask import render_template
+from app.database.models.post import Post
 
 
 @bp.route('/')
 def index():
-    return render_template("landing_page.html")
+    latest_posts = Post.query.order_by(Post.created_at.desc()).limit(3).all()
+    return render_template("landing_page.html", posts=latest_posts)
